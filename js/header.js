fc9ee21b5fbb31e9b95079385aec5cb0236ec3eb
Ainagul Shabdanova <shabdanova.ainagul.kk@gmail.com>
2022-06-28 14:37:25 +0600

    feat: add changes to header

--- js/header.js
+++ js/header.js
@@ -1,10 +1,12 @@
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
+const popupBtn = document.querySelector('.popup-btn');
 if (burger) {
 	burger.addEventListener("click", function (e) {
 		document.body.classList.toggle('_lock');
 		burger.classList.toggle('active');
 		menu.classList.toggle('active');
+		popupBtn.classList.toggle('unvisible');
 	});
 
 
